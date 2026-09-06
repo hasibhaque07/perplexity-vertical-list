c40b561d2d5270e774883381a2831a980ebb6873
hasib <hasibhaque07@gmail.com>
2026-09-07 02:11:49 +0600

    changed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View } from "react-native";
-import App from "./animations/26-React-Native-Masking-Onboarding-Screen/App";
+import App from "./animations/21-animated-split-button/App";
 //import VerticalList from "./animations/00-perplexity-verticallist/VerticalList";
 //import data from "./animations/00-perplexity-verticallist/mockData";
 //import VerticalList from "./practice/verticalList/VerticalList";
